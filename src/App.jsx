0eee39fde1c338ddd73c81552597772d1a59ef41
BenTAMUC <bsmithers1@leomail.tamuc.edu>
2023-11-30 18:45:59 -0600

    added did generator func + needed libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import * as ed from '@noble/ed25519';
+import { sha512 } from '@noble/hashes/sha512';
 import './App.css'
 
 function App() {
@@ -16,20 +18,56 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>SURI DID Generator</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => createDID('https:example.com')}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+}
+
+function createDID(url) {
+  // Generate sha 512 sync
+  ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m));
+
+  // Generate a key pair
+  const privKey = ed.utils.randomPrivateKey();
+  const pubKey = ed.getPublicKey(privKey);
+
+  // remove commas from keys
+  let privKeyNoCommas = String(privKey).replace(/,/g, '');
+  let pubKeyNoCommas = String(pubKey).replace(/,/g, '');
+
+  let id = "did:web:" + String(url);
+  let didDoc = {
+    "@context": [
+      "https://www.w3.org/ns/did/v1",
+      "https://w3id.org/security/suites/jws-2020/v1"
+    ],
+    "id": id,
+    "verificationMethod": [
+      {
+        "id": "did:web:" + String(url) + "#key-0",
+        "type": "ed25519VerificationKey2020",
+        "controller": id,
+        "publicKeyBase58": pubKeyNoCommas
+      }
+    ],
+    "authentication": [
+      "did:web:example.com#key-0"
+    ],
+    "assertionMethod": [
+      "did:web:example.com#key-0"
+    ],
+    "keyAgreement": [
+      "did:web:example.com#key-0"
+    ]
+  }
+  console.log(JSON.stringify(didDoc, null, 2));
+
+  return didDoc;
 }
 
 export default App
